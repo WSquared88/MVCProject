ebfa07b33d00f0788164c8682d20f273fb233ef9
Wes Waligora <wsw3877@ad.rit.edu>
2016-04-12 11:18:08 -0400

    Added some more debug logs

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,35 +1,39 @@
 var requiresLogin = function(req, res, next)
 {
+	cobnsole.log("must login");
 	if(!req.session.account)
 	{
 		return res.redirect("/");
 	}
-	
+	console.log("next");
 	next();
 };
 
 var requiresLogout = function(req, res, next)
 {
+	console.log("must logout");
 	if(req.session.account)
 	{
 		return res.redirect("/view");
 	}
-	
+	console.log("next");
 	next();
 };
 
 var requiresSecure = function(req, res, next)
 {
+	console.log("checking secure");
 	if(req.headers["x-forwarded-proto"] != "https")
 	{
 		return res.redirect("https://" + req.hostname + req.url);
 	}
-	
+	console.log("next");
 	next();
 };
 
 var bypassSecure = function(req, res, next)
 {
+	console.log("no secure");
 	next();
 };
 
